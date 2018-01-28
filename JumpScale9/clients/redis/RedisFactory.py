bf868eccfa5b91ec21da892e37e6edc93e406882
andrewayoub <andrew.a.mikhaeel@gmail.com>
2018-01-28 14:14:55 +0200

    fix j.core.db dependencies

--- JumpScale9/clients/redis/RedisFactory.py
+++ JumpScale9/clients/redis/RedisFactory.py
@@ -164,7 +164,6 @@ class RedisFactory:
             "redis-server --port 6379 --unixsocket %s/redis.sock --maxmemory 100000000 --daemonize yes" % tmpdir)
         limit_timeout = time.time() + timeout
         while time.time() < limit_timeout:
-            j.core.db = self.get4core()
             if j.core.db:
                 break
             time.sleep(2)
