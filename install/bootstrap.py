aee5720fb3b08f761cdb62f8a4324aa104fa5194
Jan DeLandt <jan@bettertoken.com>
2018-08-09 09:24:24 +0100

    whitespace cleanup, also make conditional with if __name__ == __main__ because its preventing and prohibiting tests from running due to InstallTools.py not being found.issue #29

--- install/bootstrap.py
+++ install/bootstrap.py
@@ -3,52 +3,53 @@
 import os
 import time
 import argparse
-
-
-if "JUMPSCALEBRANCH" in os.environ:
-    branch = os.environ["JUMPSCALEBRANCH"]
-else:
-    branch = "master"
-
-if "TMPDIR" not in os.environ:
-    os.environ["TMPDIR"] = "/tmp"
-    # raise RuntimeError("TMPDIR should be there")
-
-tmpdir = os.environ["TMPDIR"]
-
-
-print("bootstrap installtools in dir %s and use branch:'%s'" % (tmpdir, branch))
-
-# GET THE MAIN INSTALL TOOLS SCRIPT
-
-pathcheck = "%s/InstallTools.py" % os.path.abspath(os.curdir)
-if os.path.exists(pathcheck):
-    path = pathcheck
-else:
-    path = "%s/InstallTools.py" % tmpdir
-
-if not os.path.exists(path):
-    raise RuntimeError("Cannot find:%s" % path)
-
-os.chdir(tmpdir)
-
 from importlib import util
-spec = util.spec_from_file_location("InstallTools", path)
 
-InstallTools = spec.loader.load_module()
+if __name__ == '__main__':
+    if "JUMPSCALEBRANCH" in os.environ:
+        branch = os.environ["JUMPSCALEBRANCH"]
+    else:
+        branch = "master"
 
-do = InstallTools.do
+    if "TMPDIR" not in os.environ:
+        os.environ["TMPDIR"] = "/tmp"
+        # raise RuntimeError("TMPDIR should be there")
 
-# look at methods in https://github.com/threefoldtech/jumpscale_/jumpscale_core/blob/master/install/InstallTools.py to see what can be used
-# there are some easy methods to allow git manipulation, copy of files, execution of items
+    tmpdir = os.environ["TMPDIR"]
 
-# there are many more functions available in jumpscale
+    print("bootstrap installtools in dir %s and use branch:'%s'" %
+          (tmpdir, branch))
 
-# FROM now on there is a do. variable which has many features, please investigate
+    # GET THE MAIN INSTALL TOOLS SCRIPT
 
+    pathcheck = "%s/InstallTools.py" % os.path.abspath(os.curdir)
+    if os.path.exists(pathcheck):
+        path = pathcheck
+    else:
+        path = "%s/InstallTools.py" % tmpdir
 
-print("install jumpscale")
-do.installer.prepare()
-do.installer.installJS()
+    if not os.path.exists(path):
+        raise RuntimeError("Cannot find:%s" % path)
 
-from Jumpscale import j
+    os.chdir(tmpdir)
+
+    spec = util.spec_from_file_location("InstallTools", path)
+
+    InstallTools = spec.loader.load_module()
+
+    do = InstallTools.do
+
+    # look at methods in https://github.com/threefoldtech/jumpscale_/jumpscale_core/blob/master/install/InstallTools.py to see what can be used
+    # there are some easy methods to allow git manipulation, copy of files,
+    # execution of items
+
+    # there are many more functions available in jumpscale
+
+    # FROM now on there is a do. variable which has many features, please
+    # investigate
+
+    print("install jumpscale")
+    do.installer.prepare()
+    do.installer.installJS()
+
+    from Jumpscale import j
