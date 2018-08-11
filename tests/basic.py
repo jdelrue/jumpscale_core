bc820fda22eb62e400d240dd81142c0570016acb
Jan DeLandt <jan@bettertoken.com>
2018-08-11 08:30:38 +0100

    add test for whether prefab exists, if not exit test

--- tests/basic.py
+++ tests/basic.py
@@ -4,6 +4,9 @@ import time
 import subprocess
 from jumpscale import j
 
+if not hasattr(j.tools, "prefab"):
+    exit(0)
+
 STANDARD_CONFIG_DIR = '/opt/code/local/stdorg/config'
 
 j.sal.fs.createDir(STANDARD_CONFIG_DIR)
@@ -11,4 +14,4 @@ path = STANDARD_CONFIG_DIR
 if not j.sal.fs.exists("%s/.git" % STANDARD_CONFIG_DIR):
     j.tools.prefab.local.core.run("cd %s && git init ." % STANDARD_CONFIG_DIR)
 
-j.tools.prefab.local.core.run("js_config init -s -k /root/.ssh/id_rsa -p %s" % path)
+    j.tools.prefab.local.core.run("js_config init -s -k /root/.ssh/id_rsa -p %s" % path)
