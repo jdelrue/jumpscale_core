f9249326aba68f66b221bcf48ea15c51face3bbd
Jan DeLandt <jan@bettertoken.com>
2018-08-12 18:10:05 +0100

    attempting to reorganise dynamic bootstrap in Jumpscale__init__ to use JSLoader

--- Jumpscale/__init__.py
+++ Jumpscale/__init__.py
@@ -41,6 +41,45 @@ else:
     l.filter = []  # default filter which captures all is *
     j.logging = l
 
+    bases = {'j.core': 'Core',
+             'j.tools': 'Tools',
+             'j.sal': 'Sal',
+             'j.data': 'Data',
+    }
+
+    aliases = [
+        ('application', 'core.application'),
+        ('data', 'data.cache'),
+        ('core.state', 'tools.executorLocal.state'),
+        ('core.dirs', 'dirs'),
+    ]
+
+    modules = [
+        ('j.core',
+            { 'dirs': ('Jumpscale/core/Dirs.py',
+                    'Dirs', []),
+            }),
+        ('j.data',
+            {'types': ('Jumpscale/data/types/Types.py', 'Types', []),
+             'text': ('Jumpscale/data/text/Text.py', 'Text', []),
+             'datacache': ('Jumpscale/data/cache/Cache.py', 'Cache', []),
+            }),
+        ('j.sal',
+            { 'fs': ('Jumpscale/fs/SystemFS.py', 'SystemFS', []),
+              'process': ('Jumpscale/sal/process/SystemProcess.py',
+                            'SystemProcess', [])
+            }),
+        ('j.core',
+            { 'application': ('Jumpscale/core/Application.py',
+                    'Application', []),
+              'platformtype': ('Jumpscale/core/PlatformTypes.py',
+                    'PlatformTypes', []),
+            }),
+        ('j.tools',
+            { 'executorLocal': ('Jumpscale/tools/executor/ExecutorLocal.py',
+                    'ExecutorLocal', []),
+            })
+        ]
     for (parent, child, module, kls) in [
         ('', 'core', 'core', 'Core'),
         ('', 'tools', 'tools', 'Tools'),
@@ -50,21 +89,23 @@ else:
         ('data', 'text', 'data.text.Text', 'Text'),
         ('sal', 'fs', 'fs.SystemFS', 'SystemFS'),
         ('core', 'application', 'core.Application', 'Application'),
-        ('', 'application', 'core.application', None),
         ('data', 'datacache', 'data.cache.Cache', 'Cache'),
-        ('', 'cache', 'data.cache', None),
         ('tools', 'executorLocal', 'tools.executor.ExecutorLocal',
                                     'ExecutorLocal'),
-        ('core', 'state', 'tools.executorLocal.state', None),
         ('', 'dirs', 'core.Dirs', 'Dirs'),
-        ('core', 'dirs', 'dirs', None),
         ('core', 'platformtype', 'core.PlatformTypes', 'PlatformTypes'),
         ('sal', 'process', 'sal.process.SystemProcess', 'SystemProcess'),
+        ('', 'application', 'core.application', None),
+        ('', 'cache', 'data.cache', None),
+        ('core', 'state', 'tools.executorLocal.state', None),
+        ('core', 'dirs', 'dirs', None),
         ]:
+        pass
         add_dynamic_instance(parent, child, module, kls)
 
     DJSLoader = j._jsbase(j, 'JSLoader', [JSLoader])
     dl = DJSLoader()
+    #j = dl._dynamic_generate(j, modules, bases, aliases) 
     j = dl.dynamic_generate(basej=j) 
     j.logging.init()  # will reconfigure the logging to use the config file
     j.tools.executorLocal.env_check_init()
