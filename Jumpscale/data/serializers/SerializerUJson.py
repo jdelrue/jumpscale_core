831a741221f252741c8b0902c3ad72987ca7c94f
Jan DeLandt <jan@bettertoken.com>
2018-08-22 13:00:21 +0100

    the JSON encoder/decoder was a mess: it was incapable of dealing with objects
    to get one of the unit tests working (j.sal.processmanager.testSync)
    the objects had to be encoded properly.  sets, bytes etc.
    
    TODO: add a separate unit test

--- Jumpscale/data/serializers/SerializerUJson.py
+++ Jumpscale/data/serializers/SerializerUJson.py
@@ -1,10 +1,27 @@
-from Jumpscale import j # J due to recursive import issue
+""" okaaaay, JSON can't cope with bytes, sets, or objects, or, well...
+    anything significant, basically.  to deal with that, we use pickle
+    instead. however... pickle can't be put into strings, because it's
+    bytes.  soooo... we use base64 encoding.
 
-# try:
-#     import ujson as json
-# except BaseException:
+    below is cobbled together from a couple of sources:
+
+    https://stackoverflow.com/questions/30469575/how-to-pickle-and-unpickle-to-portable-string-in-python-3
+    https://stackoverflow.com/questions/8230315/how-to-json-serialize-sets
+"""
+
 import json
+import pickle
+import codecs
 from .SerializerBase import SerializerBase
+
+
+def as_python_object(dct):
+    print ("as_python_object", dct)
+    if '_python_object' in dct:
+        pickled = dct['_python_object']
+        unpickled = pickle.loads(codecs.decode(pickled.encode(), 'base64'))
+        return unpickled
+    return dct
 
 
 class BytesEncoder(json.JSONEncoder):
@@ -13,8 +30,13 @@ class BytesEncoder(json.JSONEncoder):
 
     def default(self, obj):
         if isinstance(obj, bytes):
-            j.logger.logger.debug('encoding bytes into %s' % self.ENCODING)
             return obj.decode(self.ENCODING)
+        if not isinstance(obj, (list, dict, str, int, float,
+                            bool, type(None))):
+            pickled = pickle.dumps(obj)
+            pickled = codecs.encode(pickled, 'base64')
+            pickled = pickled.decode()
+            return json.dumps({'_python_object': pickled})
         return json.JSONEncoder.default(self, obj)
 
 
@@ -43,4 +65,4 @@ class SerializerUJson(SerializerBase):
     def loads(self, s):
         if isinstance(s, bytes):
             s = s.decode('utf-8')
-        return json.loads(s)
+        return json.loads(s, object_hook=as_python_object)
