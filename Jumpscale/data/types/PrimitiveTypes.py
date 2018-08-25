e116694b8131ee3a65066d9774c56427da909e11
Jan DeLandt <jan@bettertoken.com>
2018-08-25 11:10:09 +0100

    convert PrimitiveTypes to j-less

--- Jumpscale/data/types/PrimitiveTypes.py
+++ Jumpscale/data/types/PrimitiveTypes.py
@@ -1,6 +1,5 @@
-# from jumpscale import j
-from Jumpscale import j
-'''Definition of several primitive type properties (integer, string,...)'''
+""" Definition of several primitive type properties (integer, string,...)
+"""
 
 
 class String():
@@ -75,7 +74,7 @@ class StringMultiLine(String):
         will do a strip on multiline
         """
         value = str(value)
-        return j.data.text.strip(value)
+        return self.j.data.text.strip(value)
 
     def toString(self, v):
         v = self.clean(v)
@@ -205,7 +204,7 @@ class Boolean():
         """
         if value in ["1", 1, True]:
             value = True
-        elif j.data.types.string.check(value) and value.strip().lower() in ["true", "yes", "y"]:
+        elif self.j.data.types.string.check(value) and value.strip().lower() in ["true", "yes", "y"]:
             value = True
         else:
             value = False
@@ -267,7 +266,7 @@ class Integer():
         return self.clean(v)
 
     def fromString(self, s):
-        return j.data.text.getInt(s)
+        return self.j.data.text.getInt(s)
 
     def get_default(self):
         return 0
@@ -325,7 +324,7 @@ class Float():
         return self.clean(v)
 
     def fromString(self, s):
-        return j.data.text.getFloat(s)
+        return self.j.data.text.getFloat(s)
 
     def get_default(self):
         return 0.0
