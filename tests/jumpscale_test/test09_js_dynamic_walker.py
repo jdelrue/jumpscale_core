d1d94b3688b1b23125fafa6c21e6b9eae1a5e494
Jan DeLandt <jan@bettertoken.com>
2018-08-27 10:02:59 +0100

    issue #101 - disabling j.tools.develop.test_js_quick unit test

--- tests/jumpscale_test/test09_js_dynamic_walker.py
+++ tests/jumpscale_test/test09_js_dynamic_walker.py
@@ -173,6 +173,12 @@ skipproperties = [
         # https://github.com/threefoldtech/jumpscale_lib/issues/100
         'j.tools.jinja2.test',
 
+        # https://github.com/threefoldtech/jumpscale_lib/issues/100
+        'j.tools.jinja2.test',
+
+        # https://github.com/threefoldtech/jumpscale_lib/issues/101
+        'j.tools.develop.test_js_quick',
+
     ]
 
 # use this for testing of a restricted set of tests
