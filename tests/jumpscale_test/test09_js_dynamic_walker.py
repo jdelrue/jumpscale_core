6577339848c003bb539743a1adad29cb221ed8de
Jan DeLandt <jan@bettertoken.com>
2018-08-27 10:29:32 +0100

    issue #95 in jumpscale_lib
    j.data.worksheets.test fails as j.tools.numtools does not appear to exist.

--- tests/jumpscale_test/test09_js_dynamic_walker.py
+++ tests/jumpscale_test/test09_js_dynamic_walker.py
@@ -170,6 +170,9 @@ skipproperties = [
         # https://github.com/threefoldtech/jumpscale_lib/issues/94
         'j.data.indexdb.test',
 
+        # https://github.com/threefoldtech/jumpscale_lib/issues/95
+        'j.data.worksheets.test',
+
         # https://github.com/threefoldtech/jumpscale_core/issues/100
         'j.tools.jinja2.test',
 
