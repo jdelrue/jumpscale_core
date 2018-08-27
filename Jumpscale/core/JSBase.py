af7d7cf4a6ede19a62de27c0ebde0a1fd5f8c185
Jan DeLandt <jan@bettertoken.com>
2018-08-27 19:41:37 +0100

    add camelcase-ing functions into JSBase.py
    this is in preparation for investigating backwards-compatible support
    for a transition to camel_case actually *inside* JSBase (or, BaseGetter).
    
    see howto/camelcase_convert.md for details, where the alternative is to
    get absolutely everybody to freeze absolutely all development, do a
    conversion, then let them carry on working

--- Jumpscale/core/JSBase.py
+++ Jumpscale/core/JSBase.py
@@ -121,6 +121,30 @@ def lookup_kls(__jsfullpath__, __jsmodulepath__, kls):
 
     return nkls
 
+def camel(s):
+    return s != s.lower() and s != s.upper() and "_" not in s
+
+def to_snake_case(not_snake_case):
+    final = ''
+    for i in range(len(not_snake_case)):
+        item = not_snake_case[i]
+        if i < len(not_snake_case) - 1:
+            next_char_will_be_underscored = (not_snake_case[i+1] == "_"  \
+                    or not_snake_case[i+1] == " " \
+                    or not_snake_case[i+1].isupper())
+        if (item == " " or item == "_") and next_char_will_be_underscored:
+            continue
+        elif (item == " " or item == "_"):
+            final += "_"
+        elif item.isupper():
+            final += "_"+item.lower()
+        else:
+            final += item
+    if final[0] == "_":
+        final = final[1:]
+    return final
+
+
 class BaseGetter(object):
     """ this is a rather.. um... ugly class that has a list of module names
         added to it, where if a property is ever referred to it will either
