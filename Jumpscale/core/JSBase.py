6342dbc13fb3431473231e66084f5bb23e817b10
Jan DeLandt <jan@bettertoken.com>
2018-08-27 20:45:50 +0100

    issue #105 - camelcase *LIVE* detection and migration.
    once started this can NOT be stopped or switched off, until all occurrences
    of a function have been properly migrated to lower_case.
    
    one *individual* function can be selected for migration, however if that
    function has not been fully migrated across all apps, switching off
    the live introspection / mitigation WILL make that application break.
    
    best to just leave this on all the time until such time as the complete
    and total migration has been done

--- Jumpscale/core/JSBase.py
+++ Jumpscale/core/JSBase.py
@@ -3,6 +3,8 @@ import os
 import sys
 import importlib.util
 import inspect
+import gc    # needed for camelcase
+import trace # needed for camelcase
 
 
 def jwalk(instance, name, start=None, end=None, stealth=False):
@@ -120,6 +122,90 @@ def lookup_kls(__jsfullpath__, __jsmodulepath__, kls):
     nkls.__jsmodulepath__ = mpathname
 
     return nkls
+
+# intrusive live introspection for camel_case transition
+# issue #105
+
+camel_case_log = {}
+
+def load_camel_case_log():
+    global camel_case_log
+    if len(camel_case_log) > 0:
+        return # loaded already
+    try:
+        with open("camel_case_log.txt") as f:
+            for l in f.readlines():
+                l = l.strip()
+                (k, v) = l.split("\t")
+                camel_case_log[k] = v
+    except (OSError, IOError):
+        pass # ok, ignore it.  empty.  first time loading
+
+load_camel_case_log()
+
+def save_camel_case_log_entry(k, v):
+    global camel_case_log
+    with open("camel_case_log.txt", "a+") as f:
+        f.write("%s\t%s\n" % (k, v))
+
+# this is taken straight out of the trace.py module
+caller_cache = {}
+def file_module_function_of(frame):
+
+    global caller_cache
+
+    code = frame.f_code
+    filename = code.co_filename
+    if filename:
+        modulename = trace._modname(filename)
+    else:
+        modulename = None
+
+    lineno = frame.f_lineno
+    funcname = code.co_name
+    clsname = None
+    if code in caller_cache:
+        if caller_cache[code] is not None:
+            clsname = caller_cache[code]
+    else:
+        caller_cache[code] = None
+        ## use of gc.get_referrers() was suggested by Michael Hudson
+        # all functions which refer to this code object
+        funcs = [f for f in gc.get_referrers(code)
+                     if inspect.isfunction(f)]
+        # require len(func) == 1 to avoid ambiguity caused by calls to
+        # new.function(): "In the face of ambiguity, refuse the
+        # temptation to guess."
+        if len(funcs) == 1:
+            dicts = [d for d in gc.get_referrers(funcs[0])
+                         if isinstance(d, dict)]
+            if len(dicts) == 1:
+                classes = [c for c in gc.get_referrers(dicts[0])
+                               if hasattr(c, "__bases__")]
+                if len(classes) == 1:
+                    # ditto for new.classobj()
+                    clsname = classes[0].__name__
+                    # cache the result - assumption is that new.* is
+                    # not called later to disturb this relationship
+                    # _caller_cache could be flushed if functions in
+                    # the new module get called.
+                    caller_cache[code] = clsname
+    if clsname is not None:
+        funcname = "%s.%s" % (clsname, funcname)
+
+    return filename, lineno, modulename, funcname
+
+
+def log_camel_case_found(obj, frame, attr):
+    global camel_case_log
+    (filename, lineno, modulename, funcname) = file_module_function_of(frame)
+    report = "%s.%s/%d:%s" % (modulename, funcname, lineno, attr)
+    # TODO: this is where the warning has to be put.
+    #print ("camel case found", report)
+    if report in camel_case_log:
+        return
+    camel_case_log[report] = filename
+    save_camel_case_log_entry(report, filename)
 
 def camel(s):
     return s != s.lower() and s != s.upper() and "_" not in s
@@ -404,6 +490,52 @@ class JSBase(BaseGetter):
         # these respectively make sure that dir / getattr are called only once
         self._child_mod_cache_checked = False # for __dir__
         self._child_toadd_cache_checked = set() # for direct getattr
+
+    def __getattr__(self, attrname):
+        """ issue #105 - okaay, so this where camelcase is to be detected,
+            and if nonCamelCase is detected, a warning will be put on the
+            console, the line number of the caller logged in a file, and
+            the alternative (camel_case) function called instead.
+
+            properties are *NOT* included, and are specifically detected
+            and excluded by inspect.isdatadescriptor / isgetsetdescriptor
+
+            the rules are:
+
+            * convert CLASS METHODS ONLY (the callee).
+              do NOT convert the caller... yet.
+            * run a program (any program): warnings will be given,
+              and also listed in a log file, along with the line numbers
+              of where they were called FROM (callers).
+            * run the automated tool <TBD> or just if feeling masochistic,
+              edit the CALLERs by hands.
+
+            do not do it the other way round: do NOT edit the callers
+            first.  the code below is SPECIFICALLY designed to have
+            CALLEEs be modified first.
+        """
+        # XXX not a good idea to do this.  once committed, REALLY have to
+        # stick with it.
+        #if not os.environ.get('CAMELCASECHECK'):
+        #    return BaseGetter.__getattribute__(self, attrname)
+
+        if not camel(attrname): # it_was_one_of_these, it's_cool.
+            return BaseGetter.__getattribute__(self, attrname)
+        # so, it's a camelCaseThing. first try and get it *as* camelCase
+        #print ("looking for", attrname)
+        try:
+            # ok try "snake" version...
+            to_snake = to_snake_case(attrname)
+            #print ("looking for snake %s" % to_snake)
+            attr = BaseGetter.__getattribute__(self, to_snake)
+        except AttributeError:
+            #print ("looking for %s failed, try %s" % (to_snake, attrname))
+            return BaseGetter.__getattribute__(self, attrname)
+        # ok that worked, so, hmmm, we should log finding the camel_case
+        #print ("looking for %s worked" % to_snake)
+        frame = inspect.currentframe().f_back
+        log_camel_case_found(self, frame, attrname)
+        return attr
 
     def _check_child_mod_cache(self, keys, toadd=None):
         """ checks the child module.  has two modes.  toadd=None will
