d19f4a2e98c0747c9d3733f9483af4a7906e1eab
Jan DeLandt <jan@bettertoken.com>
2018-09-05 12:42:53 +0100

    whitespace cleanup of JSBaseClassConfigs

--- Jumpscale/tools/configmanager/JSBaseClassConfigs.py
+++ Jumpscale/tools/configmanager/JSBaseClassConfigs.py
@@ -15,7 +15,8 @@ class _JSBaseClassConfigs:
     def __init__(self, child_class=None, single_item=False):
         """
         @param child_class: The class that this factory will create
-        @param single_item: In the case this factory will only ever return the same instance
+        @param single_item: In the case this factory will only
+                            ever return the same instance
                             set single_item to True
         """
         if child_class is not None:
@@ -30,20 +31,15 @@ class _JSBaseClassConfigs:
 
         # self.getall()
 
-    def get(
-            self,
-            instance="main",
-            data={},
-            create=True,
-            die=True,
-            interactive=True,
-            **kwargs):
-        """
-        Get an instance of the child_class set in the constructor
+    def get(self, instance="main", data={}, create=True,
+            die=True, interactive=True, **kwargs):
+        """ Get an instance of the child_class set in the constructor
 
-        @param instance: instance name to get. If an instance is already loaded in memory, return it
-        @data data: dictionary of data use to configure the instance
-        @PARAM interactive means that the config will be shown to user when new and user needs to accept
+            @param instance: instance name to get. If an instance is
+                             already loaded in memory, return it
+            @data data: dictionary of data use to configure the instance
+            @param interactive: means that the config will be shown
+                                to user when new and user needs to accept
         """
         if not create and instance not in self.list():
             if die:
@@ -51,12 +47,8 @@ class _JSBaseClassConfigs:
             else:
                 return None
 
-        return self._child_class(
-            instance=instance,
-            data=data,
-            parent=self,
-            interactive=interactive,
-            **kwargs)
+        return self._child_class(instance=instance, data=data, parent=self,
+                                 interactive=interactive, **kwargs)
 
     def exists(self, instance):
         return instance in self.list()
@@ -65,7 +57,8 @@ class _JSBaseClassConfigs:
         return self.get(instance=instance, data=data, create=True)
 
     def reset(self):
-        self.j.tools.configmanager.delete(location=self.__jslocation__, instance="*")
+        self.j.tools.configmanager.delete(location=self.__jslocation__,
+                                          instance="*")
         self.getall()
 
     def delete(self, instance="", prefix=""):
@@ -81,7 +74,8 @@ class _JSBaseClassConfigs:
 
     def list(self, prefix=""):
         res = []
-        for item in self.j.tools.configmanager.list(location=self.__jslocation__):
+        items = self.j.tools.configmanager.list(location=self.__jslocation__)
+        for item in items:
             if prefix != "":
                 if item.startswith(prefix):
                     res.append(item)
@@ -91,7 +85,8 @@ class _JSBaseClassConfigs:
 
     def getall(self):
         res = []
-        for name in self.j.tools.configmanager.list(location=self.__jslocation__):
+        items = self.j.tools.configmanager.list(location=self.__jslocation__)
+        for name in items:
             res.append(self.get(name, create=False))
         return res
 
