e14e314b3a4a43d7203242876032bf5dc9575633
Jan DeLandt <jan@bettertoken.com>
2018-09-06 14:57:23 +0100

    refactor etcd unit test to use setUp/tearDown

--- tests/jumpscale_test/test11_keyvalue_stores.py
+++ tests/jumpscale_test/test11_keyvalue_stores.py
@@ -2,7 +2,7 @@ import unittest
 from .testcases_base import TestcasesBase
 
 
-class TestJDataTypes(TestcasesBase):
+class TestEtcdNamespace(TestcasesBase):
 
     def test001_etcd_namespaces(self):
         etcd = self.j.clients.etcd.get()
@@ -15,25 +15,39 @@ class TestJDataTypes(TestcasesBase):
         etcd.namespace_del(ns)
         self.assertFalse(etcd.namespace_exists(ns))
 
-    def test002_etcd_getset(self):
-        etcd = self.j.clients.etcd.get()
-        ns = self.random_string()
-        db = etcd.namespace_get(ns)
+
+class TestEtcd(TestcasesBase):
+
+    def setUp(self):
+        super().setUp()
+        self.etcd = self.j.clients.etcd.get()
+        self.ns = self.random_string()
+        self.db = self.etcd.namespace_get(self.ns)
+
+    def tearDown(self):
+        self.etcd.namespace_del(self.ns)
+        super().tearDown()
+
+    def test001_etcd_getset(self):
 
         # check get and delete on non-existent value
-        self.assertRaises(KeyError, db.get, 'hello')
-        self.assertRaises(KeyError, db.delete, 'hello')
+        self.assertRaises(KeyError, self.db.get, 'hello')
+        self.assertRaises(KeyError, self.db.delete, 'hello')
 
         # add value and check it
-        db.set('hello', b'val')
-        get = db.get('hello')
+        self.db.set('hello', b'val')
+        get = self.db.get('hello')
         self.assertTrue(get == b'val')
 
         # change value and check it
-        db.set('hello', b'newval')
-        get = db.get('hello')
+        self.db.set('hello', b'newval')
+        get = self.db.get('hello')
         self.assertTrue(get == b'newval')
 
         # delete once (should be ok), delete again (raises KeyError)
-        db.delete('hello')
-        self.assertRaises(KeyError, db.delete, 'hello') # second delete
+        self.db.delete('hello')
+        self.assertRaises(KeyError, self.db.delete, 'hello') # second delete
+
+    def test002_etcd_incr(self):
+
+        value = self.db.incr('hello')
