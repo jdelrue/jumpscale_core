2614f0585eb07b09878f56445424541e6154ccfa
Jan DeLandt <jan@bettertoken.com>
2018-09-11 16:09:15 +0000

    improve CoreDNS unit test by comparing directly against dig

--- tests/jumpscale_test/test12_j_clients_coredns.py
+++ tests/jumpscale_test/test12_j_clients_coredns.py
@@ -1,12 +1,47 @@
-""" unit test for key-value stores.
+""" unit test for j.clients.coredns
 
-    currently only does etcd, should be possible to use on redis,
-    as it has a similar high-level API
+    for this test to work, etcd and coredns must have been set up
+    and properly configured, and "dig" must have been installed
+    (apt-get install dnsutils)
 """
 
 import unittest
 from .testcases_base import TestcasesBase
 from parameterized import parameterized
+import subprocess
+
+# we're testing j.clients.coredns here, not jumpscale executor, therefore
+# use subprocess directly.
+def dig(zone, qtype):
+    args = ['dig']
+    if qtype:
+        args += ['-t', qtype]
+    args.append("@localhost")
+    args.append(zone)
+    print (args)
+    resp = subprocess.check_output(args)
+    resp = resp.decode()
+    resp = resp.split('\n')
+    resp = map(str.strip, resp) # strip whitespace
+    resp = filter(None, resp) # filter empty lines
+    resp = filter(lambda x: not x.startswith(';'), resp) # filter comments
+    return list(resp)
+
+def split_whitespace(lines):
+    return list(map(str.split, lines))
+
+def compare_output(_coredns, _dig):
+    coredns = list(map(str, _coredns)) # use CoreDNS.RR.__str__ here
+    coredns = split_whitespace(coredns)
+    dig = split_whitespace(_dig)
+    coredns.sort()
+    dig.sort()
+    _coredns = list(map(lambda x: "c: '%s'" % '\t'.join(x), coredns)) 
+    _dig = list(map(lambda x: "d: '%s'" % '\t'.join(x), dig)) 
+    _coredns = '\n'.join(_coredns)
+    _dig = '\n'.join(_dig)
+    assert coredns == dig, "coredns not same answers as dig\n" \
+                           "%s\n%s\n" % (_coredns, _dig)
 
 
 class TestCoreDNS(TestcasesBase):
@@ -15,7 +50,7 @@ class TestCoreDNS(TestcasesBase):
         super().setUp()
         self.d = self._j.clients.coredns.get()
         #self.zone = self.random_string()
-        self.zone = ''
+        self.zone = 'local/skydns'
 
     def tearDown(self):
         #self.etcd.namespace_del(self.ns)
@@ -47,10 +82,17 @@ class TestCoreDNS(TestcasesBase):
 
         if subzone:
             zone = "%s/%s" % (self.zone, subzone)
+        else:
+            zone = self.zone
+        dzone = zone.split("/")
+        dzone.reverse()
+        dzone = '.'.join(dzone)
 
-        z = d.zone_get(zone)
+        z = self.d.zone_get(zone)
 
-        print (z.get_records(subzone, qtype))
+        cdns = z.get_records('', qtype)
+        dresp = dig(dzone, qtype)
+        compare_output(cdns, dresp)
 
         return
 
